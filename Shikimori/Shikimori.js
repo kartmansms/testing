3e897733bd6e8ac488e66cd8fbec8a30fd3190ff
kartman <kartman.sms@gmail.com>
2025-03-02 20:01:48 +0300

    Update Shikimori.js

--- Shikimori/Shikimori.js
+++ Shikimori/Shikimori.js
@@ -516,7 +516,8 @@
         method: 'GET',
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            'Accept': 'application/json'
+            'Accept': 'application/json',
+            'Referer': 'https://hanime.tv/' // Добавляем заголовок Referer
         },
         success: function(response) {
             if (response.hits && response.hits.length > 0) {
