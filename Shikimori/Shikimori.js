4ec1b35c379f59e757d9724fb6af9d047912de92
kartman <124631243+kartmansms@users.noreply.github.com>
2026-06-29 13:31:06 +0300

    Update Shikimori.js

--- Shikimori/Shikimori.js
+++ Shikimori/Shikimori.js
@@ -1010,7 +1010,7 @@
 
         if (params.search) args.push('search: "' + params.search.replace(/"/g, '\\"') + '"');
         if (params.kind) args.push('kind: ' + params.kind);
-        if (params.status) args.push('status: ' + params.status);
+        if (params.status) args.push('status: "' + params.status + '"');
         if (params.season) args.push('season: "' + params.season + '"');
         if (params.genre) args.push('genre: "' + params.genre + '"');
 
