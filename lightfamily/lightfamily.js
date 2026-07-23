170112db6472330375d2a44d5741f16a58b20ac7
kartman <124631243+kartmansms@users.noreply.github.com>
2026-07-23 13:17:00 +0300

    Update lightfamily.js

--- lightfamily/lightfamily.js
+++ lightfamily/lightfamily.js
@@ -451,6 +451,15 @@
             html.remove();
         };
 
+        function openRelease(slug) {
+            Lampa.Activity.push({
+                url: '',
+                title: 'Light Family',
+                component: 'lightfamily-full',
+                slug: slug
+            });
+        }
+
         function loadData() {
             body.html('<div class="lightfamily-loader">\u0417\u0430\u0433\u0440\u0443\u0437\u043a\u0430...</div>');
 
